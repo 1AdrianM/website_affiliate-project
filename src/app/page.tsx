123dc8a4336a5e70229eab12082f68faa5d87e9c
1AdrianM <20198252@itla.edu.do>
2024-05-21 20:22:07 -0400

    Adding to Production the changes: Dynamic columns Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,84 +3,31 @@ import './page.module.css'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 import Link from 'next/link';
+import { posts } from '@/postArray';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-export default function Home() {
-  
+
+export default function Home(){
+
 return(
 <main>
   <div className="container">
     <div className="row">
-      <div className="column">
-      <Image src={"/Tiegehanley.jpg"}alt='tiege hanley anti agin cream image' width={130} height={130}/>
-        <h3>Unlock your best skin yet: Discover the powe of tiege hanley skincare Product</h3>
-        <p>Tiege Hanley
-          <Link href='https://amzn.to/3V5AFYi' target='_blank'>
-            <FontAwesomeIcon className="icon" icon={faArrowRight}/>
-            </Link>
-        </p>
-      </div>
-      <div className="column">
-      <Image src={"/vest.webp"} alt='Barbell Weighted Vest image' width={130} height={130}/>
+    {posts.map(post =>(
+    <div key={post.id} className="column">
+   
+     <Image src={post.img} width={130} height={130} alt={post.id.toString()} />
+      <h3 key={post.id}>{post.title}</h3>
+      <p>{post.descr_link}
+        <Link href={post.href}target='_blank'>
+          <FontAwesomeIcon className="icon" icon={faArrowRight} />
+        </Link>
+      </p>
+</div>
+))}
+    
 
-  <h3>Elevate Your Exercise Routine</h3>
-        <p>Try Cap Barbell Adjustable Weighted Vest
-          <Link href='https://amzn.to/4bN2VUW' target='_blank'> 
-
-            <FontAwesomeIcon className="icon"icon={faArrowRight}/>
-            </Link>
-        </p>
-      </div>
-      <div className="column">
-      <Image src={"/Ap.png"} alt='Alpha Mindset book image' width={130} height={130}/>
-
-      <h3>Alpha-Mindset Guide
-
-        </h3>
-        <p>Alpha Mindset - A Guide For Men: How to Build Self-Confidence, Dream Big, Overcome Fear, And Build Better Relationship
-          <Link href='https://amzn.to/4dRxGdd' target='_blank'>
-            <FontAwesomeIcon className="icon" icon={faArrowRight}/>
-            </Link>
-        </p>
-      </div>
-      <div className="column">
-      <Image src={"/med.jpg"} alt='Marcus Aurelios Meditations image'width={130} height={130}/>
-
-<h3> Marcus Aurelios - Meditations
-          </h3>
-        <p>Here You can find the Best Seller Book
-          <Link href='https://amzn.to/3V8t9Me' target='_blank'>
-            <FontAwesomeIcon className="icon" icon={faArrowRight}/>
-            </Link>
-        </p>
-      </div>
-      <div className="column">
-      <Image src={"/set.jpg"} alt='weight rack image'width={130} height={130}/>
-
-<h3>CAP Barbell Dumbbell Set with Rack | Multiple Options in 150lbs and 210lbs </h3>
-        <p> Find the Best Dumbbel Set with Rack Click Here
-       <Link href='https://amzn.to/4asurWT' target='_blank'>
-            <FontAwesomeIcon className="icon" icon={faArrowRight}/>
-            </Link>
-        </p>
-      </div>
-      <div className="column">
-      <Image src={"/stop.jpg"} alt='book image' width={130} height={130}/>
-
-<h3> Stop Overthinking: 23 Techniques to Relieve Stress...</h3>
-        <p>Click here if you want to see more
-          <Link href='https://amzn.to/4dPIf0A' target='_blank'>
-            <FontAwesomeIcon className="icon" icon={faArrowRight}/>
-            </Link>
-        </p>
-      </div>
     </div>
   </div>
-</main>
-  
-  
-  
-  
-  
-  
+</main>  
   );
 }
---
+++ src/postArray.ts
@@ -0,0 +1,49 @@
+interface Post{
+    id:number;
+    img:string;
+    href:string;
+    title:string;
+    descr_link:string;
+}
+
+export const posts:Post[] = [{
+    id: 1,
+    href: "https://amzn.to/3V5AFYi",
+    title: "Unlock your best skin yet: Discover the powe of tiege hanley skincare routines",
+    descr_link: "Tiege Hanley",
+    img: "/Tiegehanley.jpg"
+},
+{
+    id: 2,
+    href: "https://amzn.to/4bN2VUW",
+    title: "Elevate Your Exercise Routine",
+    descr_link: "Try Cap Barbell Adjustable Weighted Vest",
+    img: "/vest.webp"
+},
+{
+    id: 3,
+    href: "https://amzn.to/4dRxGdd",
+    title: "Alpha-Mindset Guide",
+    descr_link: "Alpha Mindset - A Guide For Men: How to Build Self-Confidence, Dream Big, Overcome Fear, And Build Better Relationship",
+    img: "/Ap.png"
+},
+{
+    id: 4,
+    href: "https://amzn.to/3V8t9Me",
+    title: "Marcus Aurelios - Meditations",
+    descr_link: "Here You can find the Best Seller Book",
+    img: "/med.jpg"
+},{
+    id: 5,
+    href: "https://amzn.to/4asurWT",
+    title: "AP Barbell Dumbbell Set with Rack | Multiple Options in 150lbs and 210lbs",
+    descr_link: "Find the Best Dumbbel Set with Rack Click Here",
+    img: "/set.jpg"
+},{
+    id: 6,
+    href: "https://amzn.to/4dPIf0A",
+    title: "Stop Overthinking: 23 Techniques to Relieve Stress...",
+    descr_link: "Click here if you want to see more",
+    img: "/stop.jpg"
+}]
+export{}
